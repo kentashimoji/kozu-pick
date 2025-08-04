48182b2c15e2480beacc582529510a61bbc228dd
kentashimoji <82485309+kentashimoji@users.noreply.github.com>
2025-08-05 08:56:16 +0900

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 # プロジェクトルート設定
-project_root = Path(__file__).resolve().parent.parent
+project_root = Path(__file__).resolve().parent
 sys.path.insert(0, str(project_root))
 
 import streamlit as st
