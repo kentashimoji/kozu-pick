4cecd318c2a04ef335a556cf93eee9c4d2358b63
kentashimoji <82485309+kentashimoji@users.noreply.github.com>
2025-08-05 07:44:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import sys
 from pathlib import Path
 
 # プロジェクトルート設定
-project_root = Path(__file__).resolve().parent
-if str(project_root) not in sys.path:
-    sys.path.insert(0, str(project_root))
+#project_root = Path(__file__).resolve().parent
+#if str(project_root) not in sys.path:
+#    sys.path.insert(0, str(project_root))
 
 import streamlit as st
 
